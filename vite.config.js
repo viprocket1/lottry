36fe1cb2df8f1e2724f4643dfe52f821885d7a3f
dam num <47239352+viprocket1@users.noreply.github.com>
2026-01-03 13:22:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Prevents creating .map files, making the browser code unreadable
+    sourcemap: false, 
+  }
 })
